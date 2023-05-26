a36b76298902f542abec69768619659eb7f6df24
snow feng <snowfeng@outlook.com>
2023-05-27 02:56:31 +0800

    update for.py

--- for.py
+++ for.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # Filename: for.py
 for i in range(1, 5):
-	print i
-	print 'This is No.',i,'times, I want to tell everybody that fcpp is a good person!'
+	print (i)
+	print ('This is No.',i,'times, I want to tell everybody that fcpp is a good person!')
 else:
-	print 'The for loop is over'
+	print ('The for loop is over')
 
