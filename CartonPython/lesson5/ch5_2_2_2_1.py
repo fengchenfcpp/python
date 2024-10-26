f9d4aa7bc7c1efda84872295cb9480647e6bea3e
snow feng <snowfeng@outlook.com>
2024-10-26 13:51:53 +0800

    update

--- CartonPython/lesson5/ch5_2_2_2_1.py
+++ CartonPython/lesson5/ch5_2_2_2_1.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 # Filename: ch5_2_2_2_1.py
 
-for item in range(10):
+for item in range(100):
     print(item)
-    if item ==30:
+    if item ==99:
         break
 else:
     print('For Over')
