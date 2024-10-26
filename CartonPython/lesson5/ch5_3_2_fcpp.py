c7305b12442aaea7e9d68a920b1fe822b9bb95ae
Chen Feng <fengchenfcpp@hotmail.com>
2024-10-26 13:56:28 +0800

    rename

--- CartonPython/lesson5/ch5_3_2_fcpp.py
+++ CartonPython/lesson5/ch5_3_2_fcpp.py
@@ -2,6 +2,6 @@
 # Filename: ch5_3_2.py
 
 for item in range(10):
-    if item == 2 or item == 4 or item == 6 or item == 8:
+    if item ==0 or item == 2 or item == 4 or item == 6 or item == 8:
         continue
     print(item)
